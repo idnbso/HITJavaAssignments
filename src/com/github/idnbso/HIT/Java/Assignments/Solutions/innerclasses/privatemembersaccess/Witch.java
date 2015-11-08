c113437b3ce2af9d5a8667587b8e9a0f28343e9b
idnbso <idanbusso@gmail.com>
2015-11-08 23:36:04 +0200

    3rd Week Assignments

---
+++ src/com/github/idnbso/HIT/Java/Assignments/Solutions/innerclasses/privatemembersaccess/Witch.java
@@ -0,0 +1,49 @@
+package com.github.idnbso.hit.java.assignments.solutions.innerclasses.privatemembersaccess;
+
+public class Witch
+{
+    private String myRecipe;
+    private Caldron caldron;
+        
+    public Witch(String myRecipe)
+    {
+        super();
+        this.myRecipe = myRecipe;
+    }
+
+    public String getMyRecipe()
+    {
+        return myRecipe;
+    }
+
+    public void setMyRecipe(String myRecipe)
+    {
+        this.myRecipe = myRecipe;
+    }
+
+    public void setCaldron(Caldron caldron)
+    {
+        this.caldron = caldron;
+    }
+
+    public void castRecipe(String recipe)
+    {
+        caldron.currentRecipe = recipe;
+    }
+    
+    public void castRecipe()
+    {
+        caldron.currentRecipe = myRecipe;
+    } 
+    
+    class Caldron
+    {
+        private String currentRecipe = "None";
+
+        public String getCurrentRecipe()
+        {
+            return currentRecipe;
+        }
+        
+    }
+}
